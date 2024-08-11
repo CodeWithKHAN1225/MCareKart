7132a03c1a0212d9409caa2672948405a062fe2b
Sadiq Khan <sadiqkhantom@gmail.com>
2024-08-11 12:29:01 +0530

    updated program.cs

--- Program.cs
+++ Program.cs
@@ -29,6 +29,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Product}/{action=ProductsList}/{id?}");
+    pattern: "{controller=Account}/{action=Login}/{id?}");
 
 app.Run();
